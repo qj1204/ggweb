f016f6986a0fa30d22ee6100a80a44d2d8890cd3
xiaoxin <1429030919@qq.com>
2024-06-07 17:30:12 +0800

    day4-路由分组

--- ggin/ggweb.go
+++ ggin/ggweb.go
@@ -1,6 +1,7 @@
 package ggin
 
 import (
+	"log"
 	"net/http"
 )
 
@@ -9,12 +10,24 @@ type HandlerFunc func(c *Context)
 
 // Engine 实现了http.Handler接口
 type Engine struct {
-	router *router // 路由和处理函数映射表
+	router *router
+	*RouterGroup
+	groups []*RouterGroup
+}
+
+type RouterGroup struct {
+	prefix      string
+	middlewares []HandlerFunc // support middleware
+	parent      *RouterGroup  // support nesting
+	engine      *Engine       // all groups share an Engine instance
 }
 
 // New 创建一个Engine实例
 func New() *Engine {
-	return &Engine{router: newRouter()}
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
 }
 
 // ServeHTTP 解析请求的路径，查找路由映射表，如果查到，就执行注册的处理方法。如果查不到，就返回 404 NOT FOUND
@@ -23,19 +36,34 @@ func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	engine.router.handle(c)
 }
 
+// Group is defined to create a new RouterGroup
+// remember all groups share the same Engine instance
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := group.engine
+	newGroup := &RouterGroup{
+		prefix: group.prefix + prefix,
+		parent: group,
+		engine: engine,
+	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
+}
+
 // addRoute 将请求方式和请求路径以及处理方法添加到路由映射表
-func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
-	engine.router.addRoute(method, pattern, handler)
+func (group *RouterGroup) addRoute(method string, comp string, handler HandlerFunc) {
+	pattern := group.prefix + comp
+	log.Printf("Route %4s - %s", method, pattern)
+	group.engine.router.addRoute(method, pattern, handler)
 }
 
 // GET defines the method to add GET request
-func (engine *Engine) GET(pattern string, handler HandlerFunc) {
-	engine.addRoute("GET", pattern, handler)
+func (group *RouterGroup) GET(pattern string, handler HandlerFunc) {
+	group.addRoute("GET", pattern, handler)
 }
 
 // POST defines the method to add POST request
-func (engine *Engine) POST(pattern string, handler HandlerFunc) {
-	engine.addRoute("POST", pattern, handler)
+func (group *RouterGroup) POST(pattern string, handler HandlerFunc) {
+	group.addRoute("POST", pattern, handler)
 }
 
 // Run defines the method to start a http server
--- main.go
+++ main.go
@@ -7,22 +7,34 @@ import (
 
 func main() {
 	r := ggin.New()
-	r.GET("/", func(c *ggin.Context) {
-		c.HTML(http.StatusOK, "<h1>Hello ggin</h1>")
+	r.GET("/index", func(c *ggin.Context) {
+		c.HTML(http.StatusOK, "<h1>Index Page</h1>")
 	})
-	r.GET("/hello", func(c *ggin.Context) {
-		// expect /hello?name=gginktutu
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
-	})
-	r.GET("/hello/:name", func(c *ggin.Context) {
-		// expect /hello/geektutu
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
-	})
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(c *ggin.Context) {
+			c.HTML(http.StatusOK, "<h1>Hello ggin</h1>")
+		})
 
-	r.GET("/assets/*filepath", func(c *ggin.Context) {
-		c.JSON(http.StatusOK, ggin.H{"filepath": c.Param("filepath")})
-	})
+		v1.GET("/hello", func(c *ggin.Context) {
+			// expect /hello?name=gginktutu
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+		})
+	}
+	v2 := r.Group("/v2")
+	{
+		v2.GET("/hello/:name", func(c *ggin.Context) {
+			// expect /hello/gginktutu
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+		v2.POST("/login", func(c *ggin.Context) {
+			c.JSON(http.StatusOK, ggin.H{
+				"username": c.PostForm("username"),
+				"password": c.PostForm("password"),
+			})
+		})
+
+	}
 
 	r.Run(":9999")
-
 }
